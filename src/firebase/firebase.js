99aadb0ac2a0c113fad133e206b43c663f06e733
Felix-fale <falefelix23@gmail.com>
2023-03-28 20:16:17 +0000

    fix bug firestore database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBAUg-XgAM4rBC7MyjNlgcqGQcuFgXOJqA",
@@ -10,6 +12,17 @@ const firebaseConfig = {
   appId: "1:388034330386:web:6a6771e21056ca0a81fdcb",
 };
 
+// const firebaseConfig = {
+//   apiKey: "AIzaSyByzEwppuR5d-1fzZ1WK0gmgp6onDspcoM",
+//   authDomain: "testt-621b4.firebaseapp.com",
+//   projectId: "testt-621b4",
+//   storageBucket: "testt-621b4.appspot.com",
+//   messagingSenderId: "1035556834714",
+//   appId: "1:1035556834714:web:f008a6cc3da21d2c07fe23",
+// };
+
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const auth = getAuth();
+export const auth = getAuth(app);
+export const storage = getStorage(app);
+export const db = getFirestore(app);
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,13 +1,72 @@
 import Add from "../img/addAvatar.png";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { auth, db, storage } from "../firebase/firebase";
+import { useState } from "react";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { doc, setDoc } from "firebase/firestore";
 
 function Register(props) {
+  const [err, setErr] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const displayName = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+    const file = e.target[3].files[0];
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          // progress
+        },
+        (error) => {
+          // error
+          setErr(true);
+        },
+        async () => {
+          // success
+          const downloadURL = await getDownloadURL(uploadTask.snapshot.ref);
+
+          await updateProfile(user, {
+            displayName,
+            photoURL: downloadURL,
+          });
+
+          //create user on firestore
+          await setDoc(doc(db, "users", user.uid), {
+            displayName,
+            email,
+            photoURL: downloadURL,
+            uid: user.uid,
+          });
+        }
+      );
+    } catch (err) {
+      setErr(true);
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <div className="formContainer">
         <div className="formWrapper">
           <span className="logo">Chat app</span>
           <span className="title">Register</span>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input type="text" placeholder="display name" />
             <input type="email" placeholder="email" />
             <input type="password" placeholder="password" />
@@ -17,6 +76,7 @@ function Register(props) {
               <span>Add an Avatar</span>
             </label>
             <button>Sign up</button>
+            {err && <span>Something went wrong</span>}
           </form>
           <p>You do have an account? Login</p>
         </div>
