a5322314f2689788b6ab91a35572f8a2ae3471d8
Felix-fale <falefelix23@gmail.com>
2023-03-28 22:43:36 +0000

    feat: add search user logic

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,35 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase/firebase";
 
 function Search(props) {
+  const [userName, setUserName] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const handleSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", userName)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+        console.log(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+      console.log(err);
+    }
+  };
+
+  const handleKey = (e) => {
+    e.code === "Enter" && handleSearch();
+  };
+
   return (
     <div className="search">
       <div className="searchForm">
         <input
           type="text"
           placeholder="Find a user"
-          // onKeyDown={handleKey}
-          // onChange={(e) => setUsername(e.target.value)}
+          onKeyDown={handleKey}
+          onChange={(e) => setUserName(e.target.value)}
           // value={username}
         />
       </div>
-      <div className="userChat">
-        <img
-          src="https://images.pexels.com/photos/14172204/pexels-photo-14172204.jpeg?auto=compress&cs=tinysrgb&w=400&lazy=load"
-          alt=""
-        />
-        <div className="userChatInfo">
-          <span>Tyler</span>
-        </div>
-      </div>
-      {/* {err && <span>User not found!</span>}
+      {err && <span>User not found!</span>}
       {user && (
-        <div className="userChat" onClick={handleSelect}>
+        <div className="userChat">
           <img src={user.photoURL} alt="" />
           <div className="userChatInfo">
             <span>{user.displayName}</span>
           </div>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
